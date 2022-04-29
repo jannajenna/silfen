1001df5a78ebc01884fa98e4d6ffab27ee3c8bc1
Hubert <ziemnioczek@gmail.com>
2022-04-29 19:15:00 +0200

    dynamics work for the homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,14 +1,56 @@
-let atc = document.querySelector(".hoverImg");
-let element = document.querySelector(".addtocart");
-atc.addEventListener("mouseenter", function () {
-  showaddtocart();
-});
-atc.addEventListener("mouseleave", function () {
-  hideaddtocart();
-});
-function showaddtocart() {
-  element.classList.remove("hide");
+const urlParams = new URLSearchParams(window.location.search);
+const url = `https://abarela.dk/silfen/silfen_wordpress/wp-json/wp/v2/product`;
+
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    data.forEach(showProduct);
+    // showProduct(data);
+  })
+  .catch((e) => {
+    console.error("An error occured:", e.message);
+  });
+
+function showProduct(product) {
+  // grab the template\
+  const template = document.querySelector("#template").content;
+  // clone the template
+  const copy = template.cloneNode(true);
+  // change content
+
+  copy.querySelector(".bagname").textContent = product.productname;
+  copy.querySelector(".price").textContent = "DKK " + product.price;
+  // copy.querySelector(
+  //   ".smallProduct img"
+  // ).src = `https://hubkur.dk/databases1/wp-content/uploads/2022/03/house_2.jpg`;
+  copy.querySelector(".hoverImg").src = product.image.guid;
+
+  // document.body.style.backgroundImage = "url('img_tree.png')
+
+  // ).src = `https://hubkur.dk/databases1/wp-content/uploads/2022/03/house_${product._id}.jpg`;
+  // grab parent
+  const parent = document.querySelector(".HPproducts");
+  // append
+  parent.appendChild(copy);
 }
-function hideaddtocart() {
-  element.classList.add("hide");
-}
+
+// let atc = document.querySelector(".hoverImg");
+// let element = document.querySelector(".addtocart");
+// atc.addEventListener("mouseenter", function () {
+//   showaddtocart();
+// });
+// atc.addEventListener("mouseleave", function () {
+//   hideaddtocart();
+// });
+// function showaddtocart() {
+//   element.classList.remove("hide");
+// }
+// function hideaddtocart() {
+//   element.classList.add("hide");
+// }
